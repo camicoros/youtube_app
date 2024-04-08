23c9b27559fa1a761383b72fe34a5670fbe1af83
dd.saushkin <dd.saushkin@tensor.ru>
2024-04-08 18:01:37 +0300

    Add: add resolution choice

--- main.py
+++ main.py
@@ -67,18 +67,62 @@ class App():
             self.ask_directory()
 
         try:
-            # формируем адрес
+            # получаем адрес из формы
             youtube_link = self.link_entry.get()
-            # переводи в нужный формат
+            # создаём объект для взаимодействия с библиотекой pytube
             youtube_link_obj = pytube.YouTube(youtube_link)
+            # запрашиваем у пользователя качество видео
+            video_resolution = self.ask_video_resolution(youtube_link_obj)
             # получаем ссылку на видео с самым высоким качеством
-            video = youtube_link_obj.streams.get_highest_resolution()
+            video = youtube_link_obj.streams.get_by_itag(int(str(video_resolution)))
             # скачиваем видео
             video.download(output_path=self.dir_path)
             # выводим результат
             messagebox.showinfo("Success", "Download complete")
         except Exception as exc:
             messagebox.showerror("Error", str(exc))
+
+    def ask_video_resolution(self, youtube_obj):
+        # TODO: разбить на функции
+        title = youtube_obj.title
+        streams_list = youtube_obj.streams
+
+        get_better_quality = False
+        if get_better_quality:
+            audio_variants = ""
+        else:
+            video_variants = {stream.itag: f"{stream.mime_type}__{stream.resolution}__{stream.fps}fps__{stream.filesize_mb}Mb" for stream in streams_list.filter(progressive=True)}
+            resolution_window = tk.Toplevel(self.root)
+            resolution_window.title("Choose resolution")
+            resolution_window.config(bg=self.bg_label_color)
+
+            lb = tk.Label(resolution_window, text=f"Select resolution for {self.format_video_name(title)}", font=("Arial,15,bold"), background=self.bg_label_color)
+            lb.place(x=100, y=40)
+
+            resolution_itag = tk.StringVar(value=str(list(video_variants)[0]))
+            row = 3
+            for video_itag, video_description in video_variants.items():
+                rad_b = tk.Radiobutton(
+                    resolution_window, text=video_description, value=video_itag, variable=resolution_itag,
+                    font=("Arial,15,bold"), background=self.bg_label_color)
+                rad_b.place(x=100, y=row*40)
+                row += 1
+
+            # Кнопка скачивания
+            btn = tk.Button(resolution_window, text="Choose resolution", font=("Arial,10,bold"), bd=4, command=lambda: resolution_window.destroy())
+            btn.place(x=100, y=row*40)
+
+            # подгоняем высоту окна
+            resolution_window.geometry(f"600x{(row+3)*40}")
+            self.root.resizable(False, False)
+
+            # захватываем фокус
+            resolution_window.transient(self.root)
+            resolution_window.grab_set()
+            self.root.wait_window(resolution_window)
+
+        return resolution_itag.get()
+
 
     def ask_directory(self):
         """
@@ -94,6 +138,12 @@ class App():
         if len(self.dir_path) > 25:
             return f"...{self.dir_path[-25:]}"
         return self.dir_path
+
+    @staticmethod
+    def format_video_name(name):
+        if len(name) > 25:
+            return f"{name[:25]}..."
+        return name
 
 
 if __name__ == "__main__":
